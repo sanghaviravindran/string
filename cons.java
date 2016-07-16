8c0b122f6bf4335321fb288568ebbfba06191695
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-16 12:25:33 +0530

    Add files via upload

---
+++ cons.java
@@ -0,0 +1,8 @@
+public class vowelorcons {
+    public static void main(String[] args) {
+        char a='e';
+        if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+            System.out.println("vowel");
+        else System.out.println("consoant");
+    }
+}
